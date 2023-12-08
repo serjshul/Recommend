84be46ffbeb6f991b6f24386b98c4b18b6bc800e
serjshul <shulginsergey0@gmail.com>
2023-12-08 17:50:34 +0300

    Banner is edited

--- androidApp/src/main/java/com/serj/recommend/android/ui/screens/home/HomeScreen.kt
+++ androidApp/src/main/java/com/serj/recommend/android/ui/screens/home/HomeScreen.kt
@@ -12,12 +12,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.serj.recommend.android.R
 import com.serj.recommend.android.model.Category
 import com.serj.recommend.android.model.Recommendation
 import com.serj.recommend.android.ui.screens.home.categories.OrdinaryCategory
-import com.serj.recommend.android.ui.screens.home.categories.recommendations.BookItemData
-import com.serj.recommend.android.ui.screens.home.categories.recommendations.MediaItemData
+import com.serj.recommend.android.ui.screens.home.components.Banner
 
 @Composable
 fun HomeScreen(
@@ -47,8 +45,6 @@ fun HomeScreenContent(
     openScreen: (String) -> Unit,
     onRecommendationClick: ((String) -> Unit, Recommendation) -> Unit
 ) {
-    val banners = getBannersData()
-
     Scaffold() { paddingValues ->
         LazyColumn(
             modifier = modifier
@@ -57,9 +53,7 @@ fun HomeScreenContent(
                 .background(color = Color.White)
         ) {
             item {
-                BannerItem(
-                    banners = banners
-                )
+                Banner()
             }
 
             for (category in categories) {
@@ -72,104 +66,6 @@ fun HomeScreenContent(
                     )
                 }
             }
-
-
-            /*
-            item {
-                CrossingCategoryItem(
-                    title = "Serj's New Music",
-                    data = musicData[0],
-                    openScreen = openScreen,
-                    onRecommendationClick = onRecommendationClick
-                )
-            }
-
-
-
-            item {
-                GalleryCategoryItem(title = "Netflix and Chill", data = mediaData[0])
-            }
-
-             */
         }
     }
-}
-
-
-fun getBannersData(): ArrayList<BannerItemData> {
-    return arrayListOf(
-        BannerItemData(
-            "Super Weakness",
-            "There is Sirotkin's new EP about love, loneliness and tigers in dark glasses. It is just what you need on a lonely warm evenings",
-            R.drawable.banner_sirotkin
-        ),
-        BannerItemData(
-            "Goodbye, poor bitches!",
-            "Why is Doja Cat fighting with fans? What lies behind her new album \"Scarlett\"? And why do people on social media often say that she’s gone crazy?",
-            R.drawable.banner_doja_cat
-        ),
-        BannerItemData(
-            "Never Have I Ever",
-            "After a traumatic year, an Indian-American teen just wants to spruce up her social status — but everyone won't make it easy on her",
-            R.drawable.banner_never_have_i_ever
-        ),
-        BannerItemData(
-            "Aster",
-            "A cafe for every day with fresh pastries, a carefully selected wine list and coffee roasted for us by Gravitas.",
-            R.drawable.banner_aster
-        )
-    )
-}
-
-fun getMediaData(): ArrayList<List<MediaItemData>> {
-    val mediaData = arrayListOf<List<MediaItemData>>()
-
-    val mediaNetflix = listOf(
-        MediaItemData(
-            "Never Have I Ever",
-            "Series",
-            "Netflix",
-            R.drawable.cover_media_never_have_i_ever
-        ),
-        MediaItemData(
-            "Never Have I Ever",
-            "Series",
-            "Netflix",
-            R.drawable.cover_media_never_have_i_ever
-        ),
-        MediaItemData(
-            "Never Have I Ever",
-            "Series",
-            "Netflix",
-            R.drawable.cover_media_never_have_i_ever
-        )
-    )
-    mediaData.add(mediaNetflix)
-
-    return mediaData
-}
-
-fun getBooksData(): ArrayList<List<BookItemData>> {
-    val booksData = arrayListOf<List<BookItemData>>()
-
-    val booksCategory = listOf(
-        BookItemData(
-            "Norwegian Forest",
-            "Haruki Murakami",
-            R.drawable.cover_book_haruki_murakami_norwegian_forest
-        ),
-        BookItemData(
-            "The Night in Lisbon",
-            "Erich Maria Remarque",
-            R.drawable.cover_book_erich_maria_remarque_the_night_in_lisbon
-        ),
-        BookItemData(
-            "Book Thief",
-            "Markus Zusak",
-            R.drawable.cover_book_markus_zusak_book_thief
-        )
-    )
-    booksData.add(booksCategory)
-
-    return booksData
 }
---
+++ androidApp/src/main/java/com/serj/recommend/android/ui/screens/home/components/Banner.kt
@@ -0,0 +1,160 @@
+package com.serj.recommend.android.ui.screens.home.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.serj.recommend.android.R
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    var sizeImage by remember { mutableStateOf(IntSize.Zero) }
+    // TODO: save like / unlike by user
+    var isSaved by rememberSaveable { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(480.dp)
+    ) {
+        Image(
+            modifier = modifier
+                .matchParentSize()
+                .onGloballyPositioned {
+                    sizeImage = it.size
+                },
+            painter = painterResource(id = R.drawable.banner_backround),
+            contentDescription = "background",
+            contentScale = ContentScale.Crop
+        )
+
+        Box(
+            modifier = modifier
+                .matchParentSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(Color.Transparent, Color.Black),
+                        startY = sizeImage.height.toFloat() / 2,
+                        endY = sizeImage.height.toFloat()
+                    )
+                )
+        )
+
+        Column(
+            modifier = modifier
+                .matchParentSize()
+                .padding(bottom = 10.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = modifier.padding(start = 5.dp, end = 5.dp, bottom = 5.dp),
+                text = "Beyoncé’s \"Renaissance\"",
+                color = Color.White,
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                modifier = modifier.padding(start = 5.dp, end = 5.dp, bottom = 10.dp),
+                text = "It’s been six years since Beyoncé has graced us with new music. But the queen has returned, and her loyal Beyhive has patiently awaited “Renaissance”",
+                color = Color.White,
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Row(
+                modifier = modifier.padding(bottom = 10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                FilledTonalButton(
+                    modifier = modifier.padding(end = 5.dp),
+                    colors = ButtonDefaults.filledTonalButtonColors(
+                        containerColor = Color.White
+                    ),
+                    onClick = {
+                        // TODO: recommend on click
+                    }
+                ) {
+                    Text(
+                        modifier = modifier.padding(start = 3.dp, end = 3.dp),
+                        text = "Read",
+                        color = Color.Black,
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                OutlinedIconButton(
+                    modifier = modifier.padding(end = 2.dp),
+                    colors = IconButtonDefaults.outlinedIconButtonColors(
+                        contentColor = Color.White
+                    ),
+                    border = BorderStroke(1.dp, Color.Gray),
+                    onClick = {
+                        isSaved = !isSaved
+                    }
+                ) {
+                    Icon(
+                        imageVector = if (isSaved) Icons.Outlined.Favorite
+                        else Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Save"
+                    )
+                }
+
+                OutlinedIconButton(
+                    colors = IconButtonDefaults.outlinedIconButtonColors(
+                        contentColor = Color.White
+                    ),
+                    border = BorderStroke(1.dp, Color.Gray),
+                    onClick = {
+                        // TODO: recommend on click
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Clear,
+                        contentDescription = "Remove"
+                    )
+                }
+            }
+        }
+    }
+}
