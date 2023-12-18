f20472a7c227edc3bf4d987f1716ed967b7f4a4f
boringx <perkeboring@gmail.com>
2023-12-18 11:29:21 +0300

    need to understand how to work with data-layer in Android (my realisations before things was unstructured and a little bit chaotic)

---
+++ androidApp/src/main/java/com/serj/recommend/android/repository/user/ProfileRemoteDataSource.kt
@@ -0,0 +1,5 @@
+package com.serj.recommend.android.repository.user
+
+class ProfileRemoteDataSource {
+
+}
---
+++ androidApp/src/main/java/com/serj/recommend/android/repository/user/ProfileRepository.kt
@@ -0,0 +1,7 @@
+package com.serj.recommend.android.repository.user
+
+class ProfileRepository(
+    profileRemoteDataSource: ProfileRemoteDataSource,
+    profileLocalDataSource: ProfileLocalDataSource
+) {
+}
