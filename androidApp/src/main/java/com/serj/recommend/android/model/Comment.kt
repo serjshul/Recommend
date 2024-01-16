f783975d65636cc340ea392422d00dfddee52bcf
serjshul <shulginsergey0@gmail.com>
2024-01-16 11:14:11 +0300

    Comment data class was updated

--- androidApp/src/main/java/com/serj/recommend/android/model/Comment.kt
+++ androidApp/src/main/java/com/serj/recommend/android/model/Comment.kt
@@ -5,11 +5,11 @@ import java.util.Date
 
 data class Comment(
     @DocumentId val id: String? = null,
-    val recommendationId: String? = null,
-    val uid: String? = null,
+    val userId: String? = null,
+    val repliedCommentId: String? = null,
 
     val text: String? = null,
-
     val date: Date? = null,
+
     val likedBy: ArrayList<String>? = arrayListOf(),
 )
