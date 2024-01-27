163b7ec4c001ddcea9cbb839752c6004421f3aa5
boringx <perkeboring@gmail.com>
2024-01-27 19:04:01 +0300

    1 start implement profile screen 2 some refactoring

--- androidApp/src/main/java/com/serj/recommend/android/ui/components/media/CustomGlideImage.kt
+++ androidApp/src/main/java/com/serj/recommend/android/ui/components/media/CustomGlideImage.kt
@@ -1,10 +1,7 @@
 package com.serj.recommend.android.ui.components.media
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import com.bumptech.glide.integration.compose.CrossFade
@@ -13,26 +10,21 @@ import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
 import com.google.firebase.storage.StorageReference
 import com.serj.recommend.android.R
+import com.serj.recommend.android.ui.styles.Gray
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun CustomGlideImage(
-    modifier: Modifier = Modifier,
-    reference: StorageReference?
+    reference: StorageReference?,
+    modifier: Modifier = Modifier
 ) {
-    if (reference != null) {
-        GlideImage(
-            modifier = modifier,
-            model = reference,
-            loading = placeholder(ColorPainter(Color.LightGray)),
-            failure = placeholder(R.drawable.glide_failure),
-            transition = CrossFade,
-            contentScale = ContentScale.Crop,
-            contentDescription = "Glide image"
-        )
-    } else {
-        Box(
-            modifier = modifier.background(Color.LightGray)
-        )
-    }
+    GlideImage(
+        model = reference,
+        modifier = modifier,
+        loading = placeholder(ColorPainter(Gray)),
+        failure = placeholder(R.drawable.glide_failure),
+        transition = CrossFade,
+        contentScale = ContentScale.Crop,
+        contentDescription = "Glide image"
+    )
 }
