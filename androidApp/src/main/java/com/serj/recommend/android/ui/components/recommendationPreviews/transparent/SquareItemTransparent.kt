64e0d65cc8a3b4ae0f46405fdb786669ffeec0e2
serjshul <shulginsergey0@gmail.com>
2024-02-03 20:01:00 +0300

    Added type to SquareItemTransparent

--- androidApp/src/main/java/com/serj/recommend/android/ui/components/recommendationPreviews/transparent/SquareItemTransparent.kt
+++ androidApp/src/main/java/com/serj/recommend/android/ui/components/recommendationPreviews/transparent/SquareItemTransparent.kt
@@ -1,8 +1,11 @@
 package com.serj.recommend.android.ui.components.recommendationPreviews.transparent
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
@@ -10,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,6 +23,7 @@ import com.google.firebase.storage.StorageReference
 import com.serj.recommend.android.common.ext.recommendationCoverShape
 import com.serj.recommend.android.model.Recommendation
 import com.serj.recommend.android.ui.components.media.CustomGlideImage
+import com.serj.recommend.android.ui.styles.primary
 
 @Composable
 fun SquareItemTransparent(
@@ -33,11 +38,12 @@ fun SquareItemTransparent(
     if (title != null && creator != null) {
         Column(
             modifier = modifier
-                .height(270.dp)
+                .height(300.dp)
                 .width(200.dp)
         ) {
             CustomGlideImage(
                 modifier = Modifier
+                    .padding(bottom = 2.dp)
                     .size(200.dp)
                     .recommendationCoverShape()
                     .clickable {
@@ -49,6 +55,20 @@ fun SquareItemTransparent(
                         }
                     },
                 reference = coverReference
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 2.dp),
+                text = "Music   /   Deep House & Progressive House & UK Bass",
+                color = primary,
+                maxLines = 1,
+                fontSize = 12.sp,
+                lineHeight = 1.2.em,
+                fontWeight = FontWeight.Bold,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Start
             )
 
             Text(
@@ -73,7 +93,7 @@ fun SquareItemTransparent(
             Text(
                 text = creator,
                 color = Color.Black,
-                fontSize = 12.sp,
+                fontSize = 14.sp,
                 lineHeight = 1.2.em,
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis
@@ -86,9 +106,10 @@ fun SquareItemTransparent(
 @Composable
 fun SquareItemTransparentPreview() {
     SquareItemTransparent(
+        modifier = Modifier.background(Color.White),
         recommendationId = "",
-        title = "Title Title Title Title Title Title Title Title Title Title Title Title",
-        creator = "Creator Creator Creator Creator Creator Creator Creator Creator Creator Creator ",
+        title = "Kyle (i found you)",
+        creator = "Fred again..",
         coverReference = null,
         openScreen = { },
         onRecommendationClick = { _: (String) -> Unit, _: Recommendation -> }
