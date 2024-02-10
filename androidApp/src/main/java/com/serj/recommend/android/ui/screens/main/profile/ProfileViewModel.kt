b0d27389281320aec5180ef22bf39cfee6e7034c
boringx <perkeboring@gmail.com>
2024-02-11 00:11:34 +0300

    Create ViewModel for ProfileScreen, and collect authenticated user from firebase

---
+++ androidApp/src/main/java/com/serj/recommend/android/ui/screens/main/profile/ProfileViewModel.kt
@@ -0,0 +1,30 @@
+package com.serj.recommend.android.ui.screens.main.profile
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.serj.recommend.android.model.User
+import com.serj.recommend.android.services.AccountService
+import com.serj.recommend.android.services.LogService
+import com.serj.recommend.android.services.StorageService
+import com.serj.recommend.android.ui.screens.RecommendViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    logService: LogService,
+    private val storageService: StorageService,
+    private val accountService: AccountService
+) : RecommendViewModel(logService) {
+    var currentUser by mutableStateOf<User?>(null)
+        private set
+
+    init {
+        launchCatching {
+            accountService.currentUser.collect { user ->
+                currentUser = user
+            }
+        }
+    }
+}
